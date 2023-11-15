ae8ed65bcc1437ec8ac84f4bfc0839e59a6c8231
Julypardo <julypardorauiz@gmail.com>
2023-11-15 11:29:36 -0500

    Vista sett.py

---
+++ sett.py
@@ -0,0 +1,23 @@
+token = 'bigdateros'
+
+whatsapp_token = 'EAAMf5hl44xwBOZBMAqqdLWMLqBWUrFMa7GNl17ItaZCsRCXP9mavPGTDeEWHYAeeZCfHVspgBUve1yhFZAyHQXl54fEnxmGf5Bem7fZCNcbJD7UxEh5vsPQCzXrCkLmi7KkBCFm2hwZATCN2uHr8mtd92NMxaXkuA84PtqpGMPAL8iR8BubcKj7ZACPMTN762Jp2ioRmTpSeUZAu7u3xMgZDZD'
+
+whatsapp_url = 'https://graph.facebook.com/v17.0/151755184695306/messages'
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "Pedro_pascal_love":801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https//ondemand.com.co"
